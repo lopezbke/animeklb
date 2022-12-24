f46b62092fef3093a034c8bc857ead175720b6bc
Kevin <kevin.lopez@email.com>
2022-12-23 22:32:13 -0400

    Added basic search functionality.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Search from "./pages/search/search.js"
 import Categories from './pages/categories/categories.js';
 
+// This App uses the Kitsu API
+// https://kitsu.docs.apiary.io/
 function App() {
   return (
     <BrowserRouter>
--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,18 +1,27 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./search.css";
 
 function Search() {
 
-  let [searchResults, setSearchResults] = useState("");
+  const jsonToArray = (json) => {
+    const arrayToReturn = [];
+    Object.keys(json.data).forEach(key => {
+      arrayToReturn.push(json.data[key]);
+    });
+    console.log(arrayToReturn);
+    return arrayToReturn;
+  };
+  let [searchResults, setSearchResults] = useState([]);
   const [searchValue, setSearchValue] = useState("");
   useEffect(() => {
-    if (searchValue) 
-    {
+    if (searchValue) {
       // TODO: Add error handling.
       fetch(`https://kitsu.io/api/edge/anime?filter[text]=${searchValue}`)
-      .then((response) => response.json())
-      .then((data) =>  setSearchResults(JSON.stringify(data)));
+        .then((response) => response.json())
+        .then((data) => setSearchResults(jsonToArray(data)))
+        .catch(error => { console.log(error) });
+    } else {
+      alert("A value must be entered in the anime search box to view new results.");
     }
   }, [searchValue])
 
@@ -22,7 +31,17 @@ function Search() {
       <label>Search Anime: </label>
       <input type="text" id="searchInput" />
       <button type="submit" onClick={() => setSearchValue(document.getElementById("searchInput").value)}>Search</button>
-      <p>{searchResults}</p>
+      <ul>
+        {searchResults.map((item, index) => {
+          return <li key={index}>
+            <div>
+              <img src={item.attributes.posterImage.tiny} alt={item.attributes.slug} />
+              <span>{item.attributes.slug}</span>
+            </div>
+          </li>
+        })
+        }
+      </ul>
     </>
   );
 };

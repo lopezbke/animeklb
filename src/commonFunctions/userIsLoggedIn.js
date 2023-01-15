470367644bf6545f2b2d3da54d50dabe430ea6cf
Kevin <kevin.lopez@email.com>
2023-01-15 17:19:58 -0400

    Set user session if  local storage is set.

--- src/commonFunctions/userIsLoggedIn.js
+++ src/commonFunctions/userIsLoggedIn.js
@@ -1,5 +1,6 @@
 function userIsLoggedIn () {
     if(localStorage.getItem("credentials")) {
+        sessionStorage.setItem("credentials", localStorage.getItem("credentials"))
         return true;
     }
     if(sessionStorage.getItem("credentials")) {
